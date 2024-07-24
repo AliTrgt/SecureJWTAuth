c2dc95d8c90d0b930144e5c7c1a0d736df6880f0
Ali Turgut <ali_trgt@outlook.com>
2024-07-24 14:54:23 +0300

    Update UserService.java
    Creating RefreshTokenSystem

--- src/main/java/com/example/SpringBootSecurity/service/UserService.java
+++ src/main/java/com/example/SpringBootSecurity/service/UserService.java
@@ -26,6 +26,30 @@ public class UserService {
         return userRepository.save(user);
     }
 
+     public Map<String,String> refresh(Map<String,String> request){
+            String refreshToken = request.get("refreshToken");
+            String username = jwtService.extractUser(refreshToken);
+            UserDetails userDetails = userRepository.findByUsername(username);
+            if(jwtService.validateToken(refreshToken,userDetails)){
+                    String newAccessToken = jwtService.generateAccessToken(username);
+                    Map<String,String> tokens = new HashMap<>();
+                    tokens.put("accessToken",newAccessToken);
+                    return  tokens;
+            }
+             throw new UsernameNotFoundException("User Not Found Exception : "+username);
+    }
+    public Map<String,String> login(AuthRequest request){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.username(),request.password()));
+        if(authentication.isAuthenticated()){
+                String accessToken = jwtService.generateAccessToken(request.username());
+                String refreshToken = jwtService.generateRefreshToken(request.password());
+                Map<String,String> tokens = new HashMap<>();
+                tokens.put("accessToken",accessToken);
+                tokens.put("refreshToken",refreshToken);
+                return tokens;
+        }
+             throw new UsernameNotFoundException("Invalid Username :"+request.username());
+    }
 
 
 }
