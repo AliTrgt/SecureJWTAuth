475a0a6ea13792efd728b574a605f0feec9aaf37
Ali Turgut <ali_trgt@outlook.com>
2024-07-24 14:57:14 +0300

    Update JwtService.java

--- src/main/java/com/example/SpringBootSecurity/service/JwtService.java
+++ src/main/java/com/example/SpringBootSecurity/service/JwtService.java
@@ -1,10 +1,8 @@
-package com.example.SpringBootSecurity.service;
+package com.example.JwtUtil.service;
 
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,25 +17,22 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    @Value("${jwt.key}")
+
+    @Value("{jwt.key}")
     private String SECRET_KEY;
 
-    private int EXPIRE_TIME = 1000 * 60 * 2;
+    private int ACCES_TOKEN_EXPIRATION_DATE = 1000 * 3600 * 1;// 1 GÜN
 
-   public boolean validateToken(String token,UserDetails userDetails){
+    private int REFRESH_TOKEN_EXPIRATION_DATE = ACCES_TOKEN_EXPIRATION_DATE * 30; // 30 gün
+
+    public boolean validateToken(String token, UserDetails userDetails){
             String username = extractUser(token);
             Date expirationDate = extractExpiration(token);
+
             return userDetails.getUsername().equals(username) && !expirationDate.before(new Date());
-   }
-    public Date extractExpiration(String token){
-        Claims claims = Jwts
-                .parserBuilder()
-                .setSigningKey(generateKey())
-                .build()
-                .parseClaimsJwt(token)
-                .getBody();
-          return claims.getExpiration();
     }
+
+
     public String extractUser(String token){
         Claims claims = Jwts
                 .parserBuilder()
@@ -48,19 +43,52 @@ public class JwtService {
 
         return claims.getSubject();
     }
-    public String generateToken(String username){
+
+
+    public Date extractExpiration(String token){
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(generateKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+
+
+    public String generateAccessToken(String username){
         Map<String,Object> claims = new HashMap<>();
-        return Jwts.builder()
-                .setSubject(username)
+
+        return Jwts
+                .builder()
                 .setClaims(claims)
+                .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_TIME ))
-                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .setExpiration(new Date(System.currentTimeMillis() + ACCES_TOKEN_EXPIRATION_DATE))
+                .signWith(generateKey())
                 .compact();
     }
+
+
+    public String generateRefreshToken(String username){
+            Map<String,Object> claims = new HashMap<>();
+                return  Jwts
+                        .builder()
+                        .setClaims(claims)
+                        .signWith(generateKey())
+                        .setSubject(username)
+                        .setIssuedAt(new Date(System.currentTimeMillis()))
+                        .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_DATE))
+                        .compact();
+    }
+
+
+
     public Key generateKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
+            byte[] hashKey = Decoders.BASE64.decode(SECRET_KEY);
+            return Keys.hmacShaKeyFor(hashKey);
     }
 
 
